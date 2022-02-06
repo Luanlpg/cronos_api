3b34fe51df3029d36788cb4417d3cec7b853da4d
Luan Santos <luanlpg13@gmail.com>
2022-02-05 22:52:05 -0300

    services app / sevice model  / services TDD

--- services/tests.py
+++ services/tests.py
@@ -3,37 +3,37 @@ from rest_framework.test import APITestCase
 from rest_framework.test import APIClient
 
 from django.contrib.auth.models import User
+from .models import Service
 
 
-class UserTests(APITestCase):
+class ServiceTests(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(
             username='zzm', password='1234xxxx')
         self.user.is_admin = True
         self.user.save()
 
-    def test_list_users(self):
+    def test_list_services(self):
         """
-        Teste de listagem de usuários.
+        Teste de listagem de serviços.
         """
         client = APIClient()
         client.force_authenticate(user=self.user)
-        response = client.get('/users/')
+        response = client.get('/services/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_create_user(self):
+    def test_create_service(self):
         """
-        Teste de criação de usuário.
+        Teste de criação de serviço.
         """
         data = {
-            "username": "teste",
-            "email": "teste@teste.com",
-            "is_staff": False
+            "name": "criação de site",
+            "description": "criação de site",
+            "category": 'dev',
+            "price": 10.00
         }
         client = APIClient()
         client.force_authenticate(user=self.user)
-        response = client.post('/users/', data, format='json')
+        response = client.post('/services/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(User.objects.count(), 2)  # 1 user created in setUp()
-        self.assertEqual(User.objects.get(
-            username='teste').email, 'teste@teste.com')
+        self.assertEqual(Service.objects.count(), 1)
