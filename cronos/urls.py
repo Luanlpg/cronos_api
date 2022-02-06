9808148a0d4e11204b6731f3526cc07d723ffa79
Luan Santos <luanlpg13@gmail.com>
2022-02-06 04:38:30 -0300

    generate documentations

--- cronos/urls.py
+++ cronos/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -26,11 +29,22 @@ from services.api.viewsets import ServiceViewSet
 from team_members.api.viewsets import TeamMemberViewSet
 from posts.api.viewsets import PostViewSet
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Cronos API",
+        default_version='v1',
+        description="API para gerenciamento de Serviços, Posts e Integrantes da Equipe, permitindo que o administrador do site consiga criar, editar, deletar e visualizar os dados através de um painel administrativo.",
+        contact=openapi.Contact(email="luanlpg13@gmail.com"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'services', ServiceViewSet)
 router.register(r'team-members', TeamMemberViewSet)
+router.register(r'posts', PostViewSet)
 router.register(r'posts', PostViewSet)
 
 urlpatterns = [
@@ -39,4 +53,10 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger',
+            cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc',
+            cache_timeout=0), name='schema-redoc'),
 ]
